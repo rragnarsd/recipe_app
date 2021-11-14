6947085185db16591b586938a99574302e0bdc91
Regina Ragnarsdottir <reginaragnarsd@gmail.com>
2021-11-14 14:24:56 +0000

    removed the animation from navbar and added stack to the -profilescreen

--- lib/custom_navbar.dart
+++ lib/custom_navbar.dart
@@ -16,40 +16,6 @@ class _CustomNavBarState extends State<CustomNavBar>
     with SingleTickerProviderStateMixin {
   int selectedIndex = 0;
 
-  late Animation<double> animation;
-  late AnimationController animController;
-
-  @override
-  void initState() {
-    super.initState();
-    animController = AnimationController(
-      reverseDuration: Duration(milliseconds: 700),
-      duration: Duration(milliseconds: 800),
-      vsync: this,
-    );
-    animation = Tween<double>(
-      begin: 0.9,
-      end: 1.05,
-    ).animate(animController)
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          animController.velocity;
-        } else if (status == AnimationStatus.dismissed) {
-          animController.forward();
-        }
-      });
-    animController.forward();
-  }
-
-  @override
-  void dispose() {
-    animController.dispose();
-    super.dispose();
-  }
-
   @override
   void _onItemTapped(int index) {
     setState(() {
@@ -76,33 +42,21 @@ class _CustomNavBarState extends State<CustomNavBar>
         showUnselectedLabels: true,
         selectedItemColor: Colors.grey.shade900,
         type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Transform.scale(
-              scale: animation.value,
-              child: Icon(UniconsLine.home),
-            ),
+            icon: Icon(UniconsLine.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Transform.scale(
-              scale: animation.value,
-              child: Icon(UniconsLine.apps),
-            ),
+            icon: Icon(UniconsLine.apps),
             label: 'Category',
           ),
           BottomNavigationBarItem(
-            icon: Transform.scale(
-              scale: animation.value,
-              child: Icon(UniconsLine.bookmark),
-            ),
+            icon: Icon(UniconsLine.bookmark),
             label: 'Saved',
           ),
           BottomNavigationBarItem(
-            icon: Transform.scale(
-              scale: animation.value,
-              child: Icon(UniconsLine.user),
-            ),
+            icon: Icon(UniconsLine.user),
             label: 'Profile',
           ),
         ],
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -8,38 +8,96 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        clipBehavior: Clip.none,
         children: [
           Image.network(
               'https://images.unsplash.com/photo-1492739159057-7d1896b3c63f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1032&q=80'),
-          Container(
-            color: Colors.white,
-            child: Column(
-              children: [
-                const Text('Salmon Steak'),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text('Dinner'),
-                ),
-                Row(
-                  children: [
-                    Row(
-                      children: [Icon(UniconsLine.clock), Text('Prep Time')],
-                    ),
-                    Row(
-                      children: [Icon(UniconsLine.clock), Text('Prep Time')],
-                    ),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Icon(UniconsLine.users_alt),
-                    Text('Sharing')
-                  ],
-                )
-              ],
+          Positioned(
+            top: 200,
+            left: 18.0,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.1,
+              padding:
+                  const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Salmon Steak',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Row(
+                        children: const [
+                          Icon(
+                            UniconsLine.share,
+                            size: 26.0,
+                          ),
+                          SizedBox(width: 5.0,),
+                          Icon(
+                            UniconsLine.bookmark,
+                            size: 26.0,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: const Text('Dinner'),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Divider(thickness: 2.0, color: Colors.grey.shade200),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(UniconsLine.clock),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text('Prep Time')
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Icon(UniconsLine.clock),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text('Prep Time')
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: const [
+                      Icon(UniconsLine.users_alt),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Text('Sharing')
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-
         ],
       ),
     );
